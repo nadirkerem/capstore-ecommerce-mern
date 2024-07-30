075e40ac7d12600f7b0d1f22f4c581c009c4a434
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-07-29 21:00:03 -0400

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import HomePage from "./pages/Home";
+
 export default function App() {
-  return <h1 className="text-3xl">Hello world!</h1>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
