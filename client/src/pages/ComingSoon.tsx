b527deb82cad5c04faf1bdff0b8d9cbcef1c1822
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-08-15 03:40:27 -0400

    update coming soon page

--- client/src/pages/ComingSoon.tsx
+++ client/src/pages/ComingSoon.tsx
@@ -10,7 +10,7 @@ export default function ComingSoonPage() {
           </span>
         </p>
         <h1 className="mt-6 text-center text-4xl font-bold">
-          The page you are looking for is under construction.
+          This page is under construction
         </h1>
         <Link to="/" className="btn btn-neutral mt-6">
           Go back home
